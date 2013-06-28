7ade3b56f774d96bd7a228e0cea463179721ab6e
Gary S. Weaver <garysweaver@gmail.com>
2013-06-28 10:52:12 -0400

    replacing secret token with random hex string

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Training::Application.config.secret_token = '81523f9dabed6ce914758756ad12dbb2cc3c9b9d8b666c4f48371a777a5fb3f7b8cddb8f1f8dbed8d461afbf3d523f8fdf4be50a70840519136035b5ba595fb4'
+
+require 'securerandom'
+Training::Application.config.secret_token = SecureRandom.hex(128)
